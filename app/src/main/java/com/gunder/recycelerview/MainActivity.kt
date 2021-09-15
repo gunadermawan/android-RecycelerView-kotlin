835e22133dad5c058fda477e063f0b612f4ddfef
Guna Dermawan <53375007+gunadermawan@users.noreply.github.com>
2021-09-15 23:42:34 +0700

    done sampai mode list

--- app/src/main/java/com/gunder/recycelerview/MainActivity.kt
+++ app/src/main/java/com/gunder/recycelerview/MainActivity.kt
@@ -2,10 +2,27 @@ package com.gunder.recycelerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var rvHeroes: RecyclerView
+    private var list: ArrayList<Hero> = arrayListOf()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        rvHeroes = findViewById(R.id.rv_heroes)
+        rvHeroes.setHasFixedSize(true)
+
+        list.addAll(HeroesData.listData)
+        showRecycelerList()
+    }
+
+    private fun showRecycelerList() {
+        rvHeroes.layoutManager = LinearLayoutManager(this)
+        val listHeroAdapter = ListHeroAdapter(list)
+        rvHeroes.adapter = listHeroAdapter
     }
 }
